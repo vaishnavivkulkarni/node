5e81678498395838a8fb227b301ffd8c30c32f5d
vaishnavivkulkarni <vaishnavivkulkarni20@gmail.com>
2025-04-30 12:03:02 +0530

    added new

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello sanju').end();
+  res.status(200).send('Hello World').end();
 });
 
 // Start the server

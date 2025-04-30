ef1467a6930ffef4311f917aaaf76fb9fb4fbb6c
vaishnavivkulkarni <vaishnavivkulkarni20@gmail.com>
2025-04-30 12:13:16 +0530

    changed in app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World').end();
+  res.status(200).send('Hello world').end();
 });
 
 // Start the server
